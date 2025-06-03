8a7f73fd4ae7cbd857467f65c6780e89b9ce471a
recitativonika <fool-body-cheddar@duck.com>
2025-06-04 06:22:41 +0700

    Chain configurations

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    name: '0G Testnet',
+    rpc: 'https://evmrpc-testnet.0g.ai',
+    chainId: 16601,
+    explorer: 'https://chainscan-galileo.0g.ai',
+    nativeTokenSymbol: '0G'
+  },
+  {
+    name: 'Monad Testnet',
+    rpc: 'https://testnet-rpc.monad.xyz',
+    chainId: 10143,
+    explorer: 'https://testnet.monadexplorer.com',
+    nativeTokenSymbol: 'MON'
+  },
+  {
+    name: 'Recall Testnet',
+    rpc: 'https://evm.testnet.recall.chain.love',
+    chainId: 2481632,
+    explorer: 'https://explorer.testnet.recall.network',
+    nativeTokenSymbol: 'RECALL'
+  },
+  {
+    name: 'Holesky',
+    rpc: 'https://ethereum-holesky-rpc.publicnode.com',
+    chainId: 17000,
+    explorer: 'https://holesky.etherscan.io',
+    nativeTokenSymbol: 'ETH'
+  }
+  // Add more chains here as needed
+];
