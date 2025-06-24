a2411a9e6b69368648f99df416146d427e53d617
recitativonika <fool-body-cheddar@duck.com>
2025-06-25 01:46:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,13 @@ module.exports = [
     nativeTokenSymbol: '0G'
   },
   {
+    name: 'MEGA Testnet',
+    rpc: 'https://carrot.megaeth.com/rpc',
+    chainId: 6342,
+    explorer: 'https://www.megaexplorer.xyz',
+    nativeTokenSymbol: 'megaETH'
+  },
+  {
     name: 'Monad Testnet',
     rpc: 'https://testnet-rpc.monad.xyz',
     chainId: 10143,
@@ -21,11 +28,60 @@ module.exports = [
     nativeTokenSymbol: 'RECALL'
   },
   {
+    name: 't1 v0.3',
+    rpc: 'https://rpc.v03.t1protocol.com',
+    chainId: 299992,
+    explorer: 'https://explorer.v03.t1protocol.com',
+    nativeTokenSymbol: 't1ETH'
+  },
+  {
+    name: 'Somnia Testnet',
+    rpc: 'https://dream-rpc.somnia.network',
+    chainId: 50312,
+    explorer: 'https://dream-rpc.somnia.network',
+    nativeTokenSymbol: 'STT'
+  },
+  {
+    name: 'Seismic devnet',
+    rpc: 'https://node-2.seismicdev.net/rpc',
+    chainId: 5124,
+    explorer: 'https://explorer-2.seismicdev.net',
+    nativeTokenSymbol: 'SeismicETH'
+  },
+  {
+    name: 'Pharos Testnet',
+    rpc: 'https://testnet.dplabs-internal.com',
+    chainId: 688688,
+    explorer: 'https://testnet.pharosscan.xyz',
+    nativeTokenSymbol: 'PHRS'
+  },
+  {
+    name: 'Hoodi',
+    rpc: 'https://rpc.hoodi.ethpandaops.io',
+    chainId: 560048,
+    explorer: 'https://hoodi.etherscan.io',
+    nativeTokenSymbol: 'HoodiETH'
+  },
+  {
     name: 'Holesky',
     rpc: 'https://ethereum-holesky-rpc.publicnode.com',
     chainId: 17000,
     explorer: 'https://holesky.etherscan.io',
-    nativeTokenSymbol: 'ETH'
+    nativeTokenSymbol: 'HoleskyETH'
+  },
+  {
+    name: 'Huddle01 dRTC Chain Testnet',
+    rpc: 'https://huddle-testnet.rpc.caldera.xyz/http',
+    chainId: 2524852,
+    explorer: 'https://huddle-testnet.explorer.caldera.xyz',
+    nativeTokenSymbol: 'HuddleETH'
+  },
+  {
+    name: 'Nexus',
+    rpc: 'https://nexus-testnet.g.alchemy.com/public',
+    chainId: 3940,
+    explorer: 'https://testnet3.explorer.nexus.xyz',
+    nativeTokenSymbol: 'NEX'
   }
   // Add more chains here as needed
 ];
